fc070157c16810ca65d4d7cdacc2649044872307
Yevhenii Tomberg <yevhenii@tomberg.com>
2022-09-22 01:42:22 +0300

    Add project files.

---
+++ assignment1_yevhenii/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace assignment1_yevhenii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clear_btn = new System.Windows.Forms.Button();
+            this.close_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.country_name_label = new System.Windows.Forms.Label();
+            this.header_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.clear_btn.Location = new System.Drawing.Point(109, 374);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(147, 30);
+            this.clear_btn.TabIndex = 0;
+            this.clear_btn.Text = "CLEAR LABEL";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
+            // close_btn
+            // 
+            this.close_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.close_btn.Location = new System.Drawing.Point(363, 374);
+            this.close_btn.Name = "close_btn";
+            this.close_btn.Size = new System.Drawing.Size(100, 30);
+            this.close_btn.TabIndex = 1;
+            this.close_btn.Text = "CLOSE";
+            this.close_btn.UseVisualStyleBackColor = true;
+            this.close_btn.Click += new System.EventHandler(this.close_btn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::assignment1_yevhenii.Properties.Resources.ukraine;
+            this.pictureBox1.Location = new System.Drawing.Point(73, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(222, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::assignment1_yevhenii.Properties.Resources.usa;
+            this.pictureBox2.Location = new System.Drawing.Point(73, 208);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(222, 125);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::assignment1_yevhenii.Properties.Resources.britain;
+            this.pictureBox3.Location = new System.Drawing.Point(540, 208);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(222, 125);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::assignment1_yevhenii.Properties.Resources.estonia;
+            this.pictureBox4.Location = new System.Drawing.Point(540, 59);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(222, 125);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // country_name_label
+            // 
+            this.country_name_label.AutoSize = true;
+            this.country_name_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.country_name_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.country_name_label.Location = new System.Drawing.Point(317, 171);
+            this.country_name_label.MinimumSize = new System.Drawing.Size(200, 50);
+            this.country_name_label.Name = "country_name_label";
+            this.country_name_label.Size = new System.Drawing.Size(200, 50);
+            this.country_name_label.TabIndex = 7;
+            this.country_name_label.Text = "label2";
+            this.country_name_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // header_label
+            // 
+            this.header_label.AutoSize = true;
+            this.header_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.header_label.Location = new System.Drawing.Point(317, 21);
+            this.header_label.MinimumSize = new System.Drawing.Size(200, 50);
+            this.header_label.Name = "header_label";
+            this.header_label.Size = new System.Drawing.Size(200, 50);
+            this.header_label.TabIndex = 8;
+            this.header_label.Text = "label1";
+            this.header_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 439);
+            this.Controls.Add(this.header_label);
+            this.Controls.Add(this.country_name_label);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.close_btn);
+            this.Controls.Add(this.clear_btn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clear_btn;
+        private System.Windows.Forms.Button close_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label country_name_label;
+        private System.Windows.Forms.Label header_label;
+    }
+}
+
---
+++ assignment1_yevhenii/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment1_yevhenii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.header_label.Text = "Click somewhere";
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.country_name_label.Text = "UKRAINE";
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            this.country_name_label.Text = "ESTONIA";
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.country_name_label.Text = "USA";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            this.country_name_label.Text = "BRITAIN";
+        }
+
+        private void close_btn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            this.country_name_label.Text = "";
+        }
+    }
+}
